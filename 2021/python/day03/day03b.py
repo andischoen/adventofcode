17d9794f8c8180d613ec090fe0906d89292bc8e5
ansch <andreas.schoenbeck@gmail.com>
2021-12-04 08:22:15 +0100

    day04 2021

--- 2021/python/day03/day03b.py
+++ 2021/python/day03/day03b.py
@@ -6,7 +6,7 @@ def getFrequency(values):
     frequencyZero = [0] * len(values[0].strip())
 
     for value in values:
-        for i in range(len(value)):
+        for i in range(len(value.strip())):
             if value[i] == '0':
                 frequencyZero[i] += 1
     
@@ -17,10 +17,34 @@ def getFrequency(values):
 
 lines = []
 
-with open("F:\\programming\\github\\adventofcode\\2021\\python\\day03.txt", 'r') as f:
+with open("C:\\python\\aoc\\adventofcode\\2021\\python\\day03\\day03.txt", 'r') as f:
     lines = f.readlines()
 
 i = 0
-while (len(lines)>1):
-    frequencyZero = getFrequency(lines)
+oxygen = [num for num in lines]
+co2 = [num for num in lines]
+while (len(oxygen)>1):
+    frequencyZero = getFrequency(oxygen)
+    print("zero frequency at pos " + str(i) + " : " + str(frequencyZero[i]))
+    if frequencyZero[i] <= 0.5:
+        oxygen = [num for num in oxygen if num[i] == '1']
+    else:
+        oxygen = [num for num in oxygen if num[i] == '0']
+    i += 1
 
+print("---------------------------")
+i = 0
+while (len(co2)>1):
+    frequencyZero = getFrequency(co2)
+    print("zero frequency at pos " + str(i) + " : " + str(frequencyZero[i]))
+    if frequencyZero[i] <= 0.5:
+        co2 = [num for num in co2 if num[i] == '0']
+    else:
+        co2 = [num for num in co2 if num[i] == '1']
+    i += 1
+
+oxyInt = int(oxygen[0],2)
+co2Int = int(co2[0],2)
+print("oxygen: " + str(oxygen[0]) + " -> " + str(oxyInt))
+print("co2: " + str(co2[0]) + " -> " + str(co2Int))
+print("together: " + str(oxyInt*co2Int))
