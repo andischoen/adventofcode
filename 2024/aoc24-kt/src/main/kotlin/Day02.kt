73735abb3088bbb259bd181aef20756cbca152f9
ansch <andreas.schoenbeck@gmail.com>
2024-12-03 18:30:51 +0100

    2024 Day02 part 2 solved

--- 2024/aoc24-kt/src/main/kotlin/Day02.kt
+++ 2024/aoc24-kt/src/main/kotlin/Day02.kt
@@ -3,7 +3,7 @@ package org.example
 import java.io.BufferedReader
 import kotlin.math.absoluteValue
 
-fun main(args: Array<String>) {
+fun main() {
     val bufferedReader: BufferedReader = object {}.javaClass.getResourceAsStream("/input.txt")!!.bufferedReader()
     val lines = bufferedReader.readLines()
 
@@ -11,29 +11,42 @@ fun main(args: Array<String>) {
     lines.forEach { line ->
         val numStrs = line.split(" ")
 
-        var isSave: Boolean = true
-        var num = numStrs[0].toInt()
-        var diff: Int
-        val isDecreasing: Boolean = (num > numStrs[1].toInt())
-        for (i in 1..<numStrs.size) {
-            diff = num - numStrs[i].toInt()
-            num = numStrs[i].toInt()
-            if (diff.absoluteValue !in 1.. 3) {
-                isSave = false
-                break
-            }
-            if(diff > 0 && !isDecreasing || diff < 0 && isDecreasing) {
-                isSave = false
-                break
-            }
-        }
+        val isSave: Boolean = determineSafety(numStrs)
 
         if (isSave) {
             cnt++
+        } else {
+            for (i in numStrs.indices) {
+                val filteredNumStrs = numStrs.filterIndexed{ index, s -> index != i }
+                if (determineSafety(filteredNumStrs)) {
+                    cnt++
+                    break
+                }
+            }
         }
     }
 
     println(cnt)
 
 
+}
+
+private fun determineSafety(numStrs: List<String>): Boolean {
+    var isSave = true
+    var num = numStrs[0].toInt()
+    var diff: Int
+    val isDecreasing: Boolean = (num > numStrs[1].toInt())
+    for (i in 1..<numStrs.size) {
+        diff = num - numStrs[i].toInt()
+        num = numStrs[i].toInt()
+        if (diff.absoluteValue !in 1..3) {
+            isSave = false
+            break
+        }
+        if (diff > 0 && !isDecreasing || diff < 0 && isDecreasing) {
+            isSave = false
+            break
+        }
+    }
+    return isSave
 }
