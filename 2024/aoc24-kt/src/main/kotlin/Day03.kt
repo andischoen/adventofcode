8c6bb4a16dca8c640253f346fed9d270fd222681
ansch <andreas.schoenbeck@gmail.com>
2024-12-08 22:54:44 +0100

    2024 Day03 - well regex it is

--- 2024/aoc24-kt/src/main/kotlin/Day03.kt
+++ 2024/aoc24-kt/src/main/kotlin/Day03.kt
@@ -10,25 +10,18 @@ fun main() {
 
 
     var res = 0
-    while(hasMul(text)) {
-        res += readMul(text)
+
+    val r = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+    val matchResults = r.findAll(text)
+    matchResults.forEach {
+        val num1 = it.groups[1]?.value?.toInt()
+        val num2 = it.groups[2]?.value?.toInt()
+
+        println(num1.toString() + ", " + num2)
+
+        res += (num1!! * num2!!)
     }
 
     println(res)
 }
 
-fun readMul(text: String): Int {
-    TODO("Not yet implemented")
-}
-
-fun hasMul(text: String): Boolean {
-    var li = i
-    li = skipUntil(text, li, 'm')
-    
-}
-
-fun skipUntil(text: String, index: Int, c: Char): Int {
-    var li = index
-    while(li < text.length && text[li] != c) li++
-    return li
-}
