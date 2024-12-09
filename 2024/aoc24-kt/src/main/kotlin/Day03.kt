75b2e45f42b59ed822e5ca1b82c40e62f0d2489e
andischoen <andreas.schoenbeck@gmail.com>
2024-12-09 09:00:09 +0100

    2024 day03 - and part2

--- 2024/aoc24-kt/src/main/kotlin/Day03.kt
+++ 2024/aoc24-kt/src/main/kotlin/Day03.kt
@@ -2,14 +2,20 @@ package org.example
 
 import java.io.BufferedReader
 
-var i = 0
-
 fun main() {
     val bufferedReader: BufferedReader = object {}.javaClass.getResourceAsStream("/input03.txt")!!.bufferedReader()
-    val text = bufferedReader.readText()
-
+    var text = bufferedReader.readText()
+    //text = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
     var res = 0
+
+    //part2
+    val excludeDontsRegex = Regex("don't\\(\\).*?do\\(\\)", RegexOption.DOT_MATCHES_ALL)
+    text = excludeDontsRegex.replace(text, "")
+    val removeLastDontRegex = Regex("don't\\(\\).*")
+    text = removeLastDontRegex.replace(text, "")
+    //part2
+
 
     val r = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
     val matchResults = r.findAll(text)
@@ -17,7 +23,7 @@ fun main() {
         val num1 = it.groups[1]?.value?.toInt()
         val num2 = it.groups[2]?.value?.toInt()
 
-        println(num1.toString() + ", " + num2)
+        // println(num1.toString() + ", " + num2)
 
         res += (num1!! * num2!!)
     }
