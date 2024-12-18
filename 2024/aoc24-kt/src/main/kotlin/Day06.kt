e3983df3013ac9a541c12710e97494476f900fa0
ansch <andreas.schoenbeck@gmail.com>
2024-12-18 22:17:33 +0100

    Day06 - refactorings

--- 2024/aoc24-kt/src/main/kotlin/Day06.kt
+++ 2024/aoc24-kt/src/main/kotlin/Day06.kt
@@ -3,22 +3,23 @@ package org.example
 class Day06: IDay {
     private var posY: Int = 0
     private var posX: Int = 0
-    private lateinit var lines: List<String>
+    private lateinit var lines: List<CharArray>
 
     override fun solve() {
-        lines = getLinesFromPuzzleInput("input06-test.txt")
+        lines = getLinesFromPuzzleInput("input06-test.txt").map { it.toCharArray()}
 
-        determineGuardPosition(lines)
+        determineGuardPosition()
 
         print("Guard position: $posX / $posY")
     }
 
-    private fun determineGuardPosition(lines: List<String>) {
+    private fun determineGuardPosition() {
         for (y in lines.indices)
             for (x in lines[y].indices)
                 if (lines[y][x] in listOf('^', 'v', '<', '>')) {
                     posX = x
                     posY = y
+                    lines[y][x] = '.'
                 }
     }
 }
