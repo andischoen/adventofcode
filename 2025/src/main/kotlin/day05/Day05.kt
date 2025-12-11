1610e5cdd9f44f88930486d9bdebb80f457741b1
andischoen <andreas.schoenbeck@gmail.com>
2025-12-11 20:05:09 +0100

    aoc25 day05 part1 done

--- 2025/src/main/kotlin/day05/Day05.kt
+++ 2025/src/main/kotlin/day05/Day05.kt
@@ -1,9 +1,21 @@
 package andi.aoc25.day05
 
 import andi.aoc25.IDay
+import andi.aoc25.getTextFromPuzzleInput
 
 class Day05 : IDay{
     override fun solve(part: Int, sample: Boolean): String {
-        TODO("Not yet implemented")
+        val filename = if(sample) "input05-sample.txt" else "input05.txt"
+        val input = getTextFromPuzzleInput(filename)
+        val inputParts = input.split(Regex("\\r\\n\\r\\n"))
+
+        val strRanges = inputParts[0].split(Regex("\\r\\n")).map { r -> r.split("-").map { limit -> limit.toLong() } }
+        val ranges = strRanges.map { limits -> limits[0]..limits[1] }
+
+        val ingredients = inputParts[1].split(Regex("\\r\\n")).map { i -> i.toLong() }
+
+        val res = ingredients.count { i -> ranges.any { r -> i in r } }
+
+        return res.toString()
     }
 }
