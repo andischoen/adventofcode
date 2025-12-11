e167ef6ccfe2a17d658af8fea74f99fae88a3d3b
andischoen <andreas.schoenbeck@gmail.com>
2025-12-11 08:54:29 +0100

    Initial commit of aoc25 day05

---
+++ 2025/src/main/kotlin/day05/Day05.kt
@@ -0,0 +1,9 @@
+package andi.aoc25.day05
+
+import andi.aoc25.IDay
+
+class Day05 : IDay{
+    override fun solve(part: Int, sample: Boolean): String {
+        TODO("Not yet implemented")
+    }
+}
