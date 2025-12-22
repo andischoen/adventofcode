4533ba966749bcbb5bf0193345a39046318e069c
andischoen <andreas.schoenbeck@gmail.com>
2025-12-22 15:57:53 +0100

    Initial commit of aoc25 day07

--- 2025/src/main/kotlin/Main.kt
+++ 2025/src/main/kotlin/Main.kt
@@ -5,6 +5,7 @@ import andi.aoc25.day03.Day03
 import andi.aoc25.day04.Day04
 import andi.aoc25.day05.Day05
 import andi.aoc25.day06.Day06
+import andi.aoc25.day07.Day07
 import java.io.BufferedReader
 
 
@@ -14,6 +15,7 @@ fun main() {
     val day04 = Day04()
     val day05 = Day05()
     val day06 = Day06()
+    val day07 = Day07()
 
 //    println("day 02, part 1 -> " + day02.solve(1))
 //    println("day 02, part 2 -> " + day02.solve(2))
@@ -22,7 +24,8 @@ fun main() {
 //    println("day 04, part 1 -> " + day04.solve(1, false))
 //    println("day 04, part 2 -> " + day04.solve(2, false))
 //    println("day 05, part 1 -> " + day05.solve(2, false))
-    println("day 06, part 1 -> " + day06.solve(2, false))
+//    println("day 06, part 1 -> " + day06.solve(2, false))
+    println("day 07, part 1 -> " + day07.solve(1, true))
 
 }
 
---
+++ 2025/src/main/kotlin/day07/Day07.kt
@@ -0,0 +1,13 @@
+package andi.aoc25.day07
+
+import andi.aoc25.IDay
+import andi.aoc25.getLinesFromPuzzleInput
+
+class Day07: IDay {
+    override fun solve(part: Int, sample: Boolean): String {
+        val filename = if(sample) "input07-sample.txt" else "input07.txt"
+        val lines = getLinesFromPuzzleInput(filename)
+
+        return ""
+    }
+}
